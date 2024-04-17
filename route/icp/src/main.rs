1fe4c103a7dabdea0b9c098a3ae7d80a4045ffd8
xsb <389483673@qq.com>
2024-04-17 14:40:55 +0800

    fix: MemoryId begin from 0 & add query log by http

--- route/icp/src/main.rs
+++ route/icp/src/main.rs
@@ -13,10 +13,12 @@ use icp_route::updates::generate_ticket::{
 };
 use icp_route::updates::{self};
 use icp_route::{periodic_task, storage, TokenResp, ICP_TRANSFER_FEE, PERIODIC_TASK_INTERVAL};
-use log::{self};
-use omnity_types::log::init_log;
+use log::{self, info};
+use omnity_types::log::{init_log, StableLogWriter};
 use omnity_types::{Chain, ChainId};
+use std::str::FromStr;
 use std::time::Duration;
+use ic_canisters_http_types::{HttpRequest, HttpResponse, HttpResponseBuilder};
 
 #[init]
 fn init(args: RouteArg) {
@@ -113,6 +115,52 @@ pub fn get_redeem_fee(chain_id: ChainId) -> Option<u64> {
             })
     })
 }
+
+#[query(hidden = true)]
+fn http_request(req: HttpRequest) -> HttpResponse {
+    if req.path() == "/logs" {
+        use serde_json;
+        let max_skip_timestamp = parse_param::<u64>(&req, "time").unwrap_or(0);
+        let offset = match parse_param::<usize>(&req, "offset") {
+            Ok(value) => value,
+            Err(err) => return err,
+        };
+        let limit = match parse_param::<usize>(&req, "limit") {
+            Ok(value) => value,
+            Err(err) => return err,
+        };
+        info!(
+            "log req, max_skip_timestamp: {}, offset: {}, limit: {}",
+            max_skip_timestamp, offset, limit
+        );
+
+        let logs = StableLogWriter::get_logs(max_skip_timestamp, offset, limit);
+        HttpResponseBuilder::ok()
+            .header("Content-Type", "application/json; charset=utf-8")
+            .with_body_and_content_length(serde_json::to_string(&logs).unwrap_or_default())
+            .build()
+    } else {
+        HttpResponseBuilder::not_found().build()
+    }
+}
+
+fn parse_param<T: FromStr>(req: &HttpRequest, param_name: &str) -> Result<T, HttpResponse> {
+    match req.raw_query_param(param_name) {
+        Some(arg) => match arg.parse() {
+            Ok(value) => Ok(value),
+            Err(_) => Err(HttpResponseBuilder::bad_request()
+                .with_body_and_content_length(format!(
+                    "failed to parse the '{}' parameter",
+                    param_name
+                ))
+                .build()),
+        },
+        None => Err(HttpResponseBuilder::bad_request()
+            .with_body_and_content_length(format!("must provide the '{}' parameter", param_name))
+            .build()),
+    }
+}
+
 
 #[pre_upgrade]
 fn pre_upgrade() {
