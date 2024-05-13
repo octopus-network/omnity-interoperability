0b6da4318779b2cbb7bf623703fd5a9ffbf4f8f4
stewart <16131485+StewartYe@users.noreply.github.com>
2024-05-13 11:31:42 +0800

    update

--- route/cdk/src/service.rs
+++ route/cdk/src/service.rs
@@ -1,6 +1,8 @@
-use ic_cdk::{init, post_upgrade, pre_upgrade};
+use ic_cdk::{init, post_upgrade, pre_upgrade, query, update};
+use ic_cdk::api::management_canister::ecdsa::{ecdsa_public_key, EcdsaPublicKeyArgument};
+use crate::Error;
 
-use crate::state::{CdkRouteState, mutate_state, read_state};
+use crate::state::{CdkRouteState, key_derivation_path, key_id, mutate_state, read_state};
 
 /*
 #[init]
@@ -29,6 +31,39 @@ fn post_upgrade() {
     set_timer_interval(Duration::from_secs(30), scan_cdk_task);*/
 }
 
+#[update(guard = "is_admin")]
+async fn init_chain_pubkey() -> String{
+    let arg = EcdsaPublicKeyArgument {
+        canister_id: None,
+        derivation_path: key_derivation_path(),
+        key_id: key_id(),
+    };
+    let res = ecdsa_public_key(arg)
+        .await
+        .map_err(|(_, e)| Error::ChainKeyError(e));
+    match res {
+        Ok((t,)) => {
+            mutate_state(|s|s.pubkey = t.public_key.clone());
+            hex::encode(t.public_key)
+        }
+        Err(e) => {
+            e.to_string()
+        }
+    }
+}
 
+#[query]
+fn pubkey() -> String{
+    let key = read_state(|s|s.pubkey.clone());
+    hex::encode(key)
+}
+
+fn is_admin() -> Result<(), String>{
+    let c = ic_cdk::caller();
+    match read_state(|s|s.admin == c) {
+        true => {Ok(())}
+        false => { Err("permission deny".to_string())}
+    }
+}
 
 ic_cdk::export_candid!();
