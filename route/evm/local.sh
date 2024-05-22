e02db2196df636b55446bb3518f67604662e4447
stewart <16131485+StewartYe@users.noreply.github.com>
2024-05-22 11:23:49 +0800

    update

--- route/evm/local.sh
+++ route/evm/local.sh
@@ -4,4 +4,4 @@ dfx start --clean --background
 dfx deploy evm_rpc --argument '(record { nodesInSubnet = 28 })'
 
 #deploy cdk route
-dfx deploy cdk_route --argument '(record { network = variant { local }; omnity_port_contract = "0x765F2c1F334E6479Be5D5F8f2E12128612f47CE3"; scan_start_height = 200000; evm_rpc_canister_addr = principal "bkyz2-fmaaa-aaaaa-qaaaq-cai";  evm_chain_id = 11155111; admin = principal "bd3sg-teaaa-aaaaa-qaaba-cai"; hub_principal = principal "bd3sg-teaaa-aaaaa-qaaba-cai"; chain_id = "cdk_sepolia"; rpc_url = "https://rpc-sepolia.rockx.com";})'
+dfx deploy evm_route --argument '(record { fee_token_id = "BTC" network = variant { local }; omnity_port_contract = "0x765F2c1F334E6479Be5D5F8f2E12128612f47CE3"; scan_start_height = 200000; evm_rpc_canister_addr = principal "bkyz2-fmaaa-aaaaa-qaaaq-cai";  evm_chain_id = 11155111; admin = principal "bd3sg-teaaa-aaaaa-qaaba-cai"; hub_principal = principal "be2us-64aaa-aaaaa-qaabq-cai"; chain_id = "cdk_sepolia"; rpc_url = "https://rpc-sepolia.rockx.com";})'
