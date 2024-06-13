75936d2a6329c706c8df3261fb6a246acd684a16
Stewart Ye <16131485+StewartYe@users.noreply.github.com>
2024-06-13 20:12:22 +0800

    update EVM_FINALIZED_CONFIRM_HEIGHT (#54)

--- route/evm/src/lib.rs
+++ route/evm/src/lib.rs
@@ -50,7 +50,7 @@ pub mod const_args {
     pub const SCAN_EVM_TASK_INTERVAL: u64 = 30;
     pub const SCAN_EVM_TASK_NAME: &str = "SCAN_EVM";
     pub const EIP1559_TX_ID: u8 = 2;
-    pub const EVM_FINALIZED_CONFIRM_HEIGHT: u64 = 12;
+    pub const EVM_FINALIZED_CONFIRM_HEIGHT: u64 = 21;
     pub const DEFAULT_EVM_TX_FEE: u32 = 200_000u32;
     pub const ADD_TOKEN_EVM_TX_FEE: u32 = 3_000_000u32;
     pub const SCAN_EVM_CYCLES: u128 = 10_000_000_000;
