555711adc38f9a5bcd09683667c7da63edc60272
Stewart Ye <16131485+StewartYe@users.noreply.github.com>
2024-07-08 15:54:40 +0800

    feature/rewrite tx hash (#87)
    * stash
    
    * add rewrite tx hash

--- route/evm/src/hub.rs
+++ route/evm/src/hub.rs
@@ -1,9 +1,9 @@
 use candid::Principal;
 
 use crate::call_error::{CallError, Reason};
-use crate::types::Topic;
-use crate::types::{ChainId, Directive};
+use crate::types::{ChainId, Directive, TicketId};
 use crate::types::{Seq, Ticket};
+use crate::types::Topic;
 
 pub async fn send_ticket(hub_principal: Principal, ticket: Ticket) -> Result<(), CallError> {
     // TODO determine how many cycle it will cost.
@@ -69,3 +69,29 @@ pub async fn query_directives(
     })?;
     Ok(data)
 }
+
+pub async fn rewrite_ticket_mint_hash(
+    hub_principal: Principal,
+    ticket_id: TicketId,
+    mint_tx_hash: String,
+) -> Result<(), CallError> {
+    let resp: (Result<(), crate::types::Error>, ) = ic_cdk::api::call::call(
+        hub_principal,
+        "update_tx_hash",
+        (
+            ticket_id,
+            mint_tx_hash
+        ),
+    )
+        .await
+        .map_err(|(code, message)| CallError {
+            method: "update_tx_hash".to_string(),
+            reason: Reason::from_reject(code, message),
+        })?;
+    resp.0.map_err(|err| CallError {
+        method: "update_tx_hash".to_string(),
+        reason: Reason::CanisterError(err.to_string()),
+    })?;
+    Ok(())
+}
+
