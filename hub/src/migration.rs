a6d72a5d26e5cbb2ad50dde80159c2ee03938fb0
Boern <boern.x@gmail.com>
2024-08-01 18:04:38 +0800

    Update migration

--- hub/src/migration.rs
+++ hub/src/migration.rs
@@ -1,49 +1,27 @@
 use crate::auth::Permission;
 use crate::lifecycle::init::InitArgs;
-use crate::memory::{self, Memory};
 
 use crate::state::HubState;
-use crate::types::{Amount, ChainMeta, ChainTokenFactor, Subscribers, TokenKey, TokenMeta};
+
 use candid::Principal;
 
-use ic_stable_structures::StableBTreeMap;
-
-use omnity_types::{ChainId, Directive, Seq, SeqKey, Ticket, TicketId, TokenId, Topic};
+use crate::self_help::AddRunesTokenReq;
+use omnity_types::{ChainId, Directive, Seq, SeqKey};
 use serde::{Deserialize, Serialize};
-
-use std::collections::{BTreeMap, HashMap};
-#[derive(Deserialize, Serialize)]
+use std::collections::{BTreeMap, BTreeSet, HashMap};
+#[derive(Deserialize, Serialize, Debug)]
 pub struct PreHubState {
-    #[serde(skip, default = "memory::init_chain")]
-    pub chains: StableBTreeMap<ChainId, ChainMeta, Memory>,
-    #[serde(skip, default = "memory::init_token")]
-    pub tokens: StableBTreeMap<TokenId, TokenMeta, Memory>,
-    #[serde(skip, default = "memory::init_chain_factor")]
-    pub target_chain_factors: StableBTreeMap<ChainId, u128, Memory>,
-    #[serde(skip, default = "memory::init_token_factor")]
-    pub fee_token_factors: StableBTreeMap<TokenKey, ChainTokenFactor, Memory>,
-    #[serde(skip, default = "memory::init_directive")]
-    pub directives: StableBTreeMap<String, Directive, Memory>,
-    #[serde(skip, default = "memory::init_dire_queue")]
-    pub dire_queue: StableBTreeMap<SeqKey, Directive, Memory>,
-    #[serde(skip, default = "memory::init_subs")]
-    pub topic_subscribers: StableBTreeMap<Topic, Subscribers, Memory>,
-    #[serde(skip, default = "memory::init_ticket_queue")]
-    pub ticket_queue: StableBTreeMap<SeqKey, Ticket, Memory>,
-    #[serde(skip, default = "memory::init_token_position")]
-    pub token_position: StableBTreeMap<TokenKey, Amount, Memory>,
-    #[serde(skip, default = "memory::init_ledger")]
-    pub cross_ledger: StableBTreeMap<TicketId, Ticket, Memory>,
-
+    // memory variable
     pub directive_seq: HashMap<String, Seq>,
     pub ticket_seq: HashMap<String, Seq>,
     pub admin: Principal,
     pub caller_chain_map: HashMap<String, ChainId>,
     pub caller_perms: HashMap<String, Permission>,
     pub last_resubmit_ticket_time: u64,
-
+    pub add_runes_token_requests: BTreeMap<String, AddRunesTokenReq>,
+    pub runes_oracles: BTreeSet<Principal>,
     pub dire_map: BTreeMap<SeqKey, Directive>,
-    pub ticket_map: BTreeMap<SeqKey, Ticket>,
+    pub ticket_map: BTreeMap<SeqKey, String>,
 }
 
 // migrate pre state to current state
@@ -57,13 +35,9 @@ pub fn migrate(pre_state: PreHubState) -> HubState {
     cur_state.caller_chain_map = pre_state.caller_chain_map;
     cur_state.caller_perms = pre_state.caller_perms;
     cur_state.last_resubmit_ticket_time = pre_state.last_resubmit_ticket_time;
+    cur_state.add_runes_token_requests = pre_state.add_runes_token_requests;
+    cur_state.runes_oracles = pre_state.runes_oracles;
     cur_state.dire_map = pre_state.dire_map;
-
-    // migrate ticket_map
-    pre_state.ticket_map.iter().for_each(|(k, v)| {
-        cur_state
-            .ticket_map
-            .insert(k.to_owned(), v.ticket_id.to_string());
-    });
+    cur_state.ticket_map = pre_state.ticket_map;
     cur_state
 }
