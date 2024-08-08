85bfbe18d3705cf8f728248688a6d4cfd3a1fdf4
Stewart Ye <16131485+StewartYe@users.noreply.github.com>
2024-08-08 12:01:45 +0800

    comp error code (#112)

--- route/evm/src/eth_common.rs
+++ route/evm/src/eth_common.rs
@@ -217,7 +217,8 @@ pub async fn broadcast(tx: Vec<u8>) -> Result<String, super::Error> {
             },
             Err(r) => {
                 if let RpcError::JsonRpcError(ref jerr) = r {
-                    if jerr.code == -32603 && jerr.message == "already known" {
+                    if (jerr.code == -32603 && jerr.message == "already known")
+                        || (jerr.code == -32010 && jerr.message == "pending transaction with same hash already exists") {
                         return Ok(hex::encode([1u8; 32]));
                     }
                 }
