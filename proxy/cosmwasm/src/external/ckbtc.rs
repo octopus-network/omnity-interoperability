b1a2865225ab53b6b84a49a0489b9c73e47cc87f
xsb <389483673@qq.com>
2024-09-02 15:52:25 +0800

    feat: trigger update ckbtc in proxy and record

--- proxy/cosmwasm/src/external/ckbtc.rs
+++ proxy/cosmwasm/src/external/ckbtc.rs
@@ -1,4 +1,5 @@
 use candid::Nat;
+use ic_btc_interface::{OutPoint, Utxo};
 use icrc_ledger_types::{
     icrc1::account::{Account, Subaccount},
     icrc2::approve::ApproveArgs,
@@ -67,21 +68,86 @@ pub async fn get_ckbtc_transaction(block_index: BlockIndex) -> Result<GetTransac
     result.0
 }
 
-// pub async fn get_account_transactions(
-//     arg: GetAccountTransactionsArgs,
-// ) -> Result<GetAccountTransactionsResult> {
-//     let ckbtc_index_principal = state::get_ckbtc_ledger_principal();
-//     let result: (std::result::Result<GetAccountTransactionsResult, Errors>,) =
-//         ic_cdk::api::call::call(ckbtc_index_principal, "get_account_transactions", (arg,))
-//             .await
-//             .map_err(|(code, message)| {
-//                 Errors::CanisterCallError(
-//                     ckbtc_index_principal.to_string(),
-//                     "get_account_transactions".to_string(),
-//                     format!("{:?}", code),
-//                     message,
-//                 )
-//             })?;
+pub async fn update_balance(args: UpdateBalanceArgs)-> Result<Vec<UtxoStatus>> {
+    let ckbtc_minter_principal = state::get_ckbtc_minter_principal();
+    let result: (std::result::Result<Vec<UtxoStatus>, UpdateBalanceError>,) =
+    ic_cdk::api::call::call(ckbtc_minter_principal, "update_balance", (args.clone(),))
+        .await
+        .map_err(|(code, msg)| {
+            Errors::CanisterCallError(
+                ckbtc_minter_principal.to_string(),
+                "get_transactions".to_string(),
+                format!("{:?}", code),
+                msg,
+            )
+        })?;
 
-//     result.0.map_err(|e| Errors::CustomError(e.to_string()))
-// }
+    result.0.map_err(|e| 
+        Errors::CkBtcUpdateBalanceError(
+            format!("{:?}", args).to_string(),
+            format!("{:?}",e).to_string()
+        )
+    )
+
+} 
+
+
+#[derive(CandidType, Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+pub struct UpdateBalanceArgs {
+    /// The owner of the account on the ledger.
+    /// The minter uses the caller principal if the owner is None.
+    pub owner: Option<Principal>,
+    /// The desired subaccount on the ledger, if any.
+    pub subaccount: Option<Subaccount>,
+}
+
+/// The outcome of UTXO processing.
+#[derive(CandidType, Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+pub enum UtxoStatus {
+    /// The UTXO value does not cover the KYT check cost.
+    ValueTooSmall(Utxo),
+    /// The KYT check found issues with the deposited UTXO.
+    Tainted(Utxo),
+    /// The deposited UTXO passed the KYT check, but the minter failed to mint ckBTC on the ledger.
+    /// The caller should retry the [update_balance] call.
+    Checked(Utxo),
+    /// The minter accepted the UTXO and minted ckBTC tokens on the ledger.
+    Minted {
+        /// The MINT transaction index on the ledger.
+        block_index: u64,
+        /// The minted amount (UTXO value minus fees).
+        minted_amount: u64,
+        /// The UTXO that caused the balance update.
+        utxo: Utxo,
+    },
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq, Eq)]
+pub enum UpdateBalanceError {
+    /// The minter experiences temporary issues, try the call again later.
+    TemporarilyUnavailable(String),
+    /// There is a concurrent [update_balance] invocation from the same caller.
+    AlreadyProcessing,
+    /// The minter didn't discover new UTXOs with enough confirmations.
+    NoNewUtxos {
+        /// If there are new UTXOs that do not have enough
+        /// confirmations yet, this field will contain the number of
+        /// confirmations as observed by the minter.
+        current_confirmations: Option<u32>,
+        /// The minimum number of UTXO confirmation required for the minter to accept a UTXO.
+        required_confirmations: u32,
+        /// List of utxos that don't have enough confirmations yet to be processed.
+        pending_utxos: Option<Vec<PendingUtxo>>,
+    },
+    GenericError {
+        error_code: u64,
+        error_message: String,
+    },
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq, Eq)]
+pub struct PendingUtxo {
+    pub outpoint: OutPoint,
+    pub value: u64,
+    pub confirmations: u32,
+}
--- proxy/cosmwasm/src/service.rs
+++ proxy/cosmwasm/src/service.rs
@@ -1,8 +1,16 @@
 use crate::*;
 use candid::Nat;
-use external::custom::{generate_ticket, TARGET_CHAIN_ID, TOKEN_ID};
+use external::{
+    ckbtc,
+    custom::{generate_ticket, TARGET_CHAIN_ID, TOKEN_ID},
+};
+use ic_cdk::post_upgrade;
 use icrc_ledger_types::icrc1::{account::Account, transfer::BlockIndex};
-use state::{contains_executed_transaction_index, insert_executed_transaction_index};
+use state::{
+    contains_executed_transaction_index, get_btc_transport_records, get_ckbtc_ledger_principal,
+    get_icp_custom_principal, insert_btc_transport_records, insert_executed_transaction_index,
+    set_ckbtc_minter_principal, BtcTransportRecord,
+};
 use utils::{nat_to_u128, nat_to_u64};
 
 #[ic_cdk::init]
@@ -11,9 +19,12 @@ pub async fn init(args: lifecycle::init::InitArgs) {
 }
 
 #[query]
-pub fn get_identity_by_osmosis_account_id(osmosis_account_id: String) -> std::result::Result<Account, String> {
+pub fn get_identity_by_osmosis_account_id(
+    osmosis_account_id: String,
+) -> std::result::Result<Account, String> {
     let address_data = AddressData::try_from(osmosis_account_id.as_str())
-        .map_err(|e| Errors::AccountIdParseError(osmosis_account_id.clone(), e.to_string())).map_err(|e| e.to_string())?;
+        .map_err(|e| Errors::AccountIdParseError(osmosis_account_id.clone(), e.to_string()))
+        .map_err(|e| e.to_string())?;
 
     Ok(Account {
         owner: ic_cdk::api::id(),
@@ -21,10 +32,99 @@ pub fn get_identity_by_osmosis_account_id(osmosis_account_id: String) -> std::re
     })
 }
 
+#[query]
+pub fn query_btc_transport_info(osmosis_account_id: String) -> Vec<BtcTransportRecord> {
+    get_btc_transport_records(osmosis_account_id)
+}
+
+#[ic_cdk::query]
+pub async fn query_status() -> (Principal, Principal) {
+    (get_icp_custom_principal(), get_ckbtc_ledger_principal())
+}
+
 #[ic_cdk::update]
 pub fn set_trigger_principal(principal: Principal) -> std::result::Result<(), String> {
-    assert!(ic_cdk::api::is_controller(&ic_cdk::caller()), "Caller is not controller");
+    assert!(
+        ic_cdk::api::is_controller(&ic_cdk::caller()),
+        "Caller is not controller"
+    );
     state::set_trigger_principal(principal);
+    Ok(())
+}
+
+#[ic_cdk::update]
+pub async fn trigger_update_balance(osmosis_account_id: String) -> std::result::Result<(), String> {
+    let address_data = AddressData::try_from(osmosis_account_id.as_str())
+        .map_err(|e| Errors::AccountIdParseError(osmosis_account_id.clone(), e.to_string()))
+        .map_err(|e| e.to_string())?;
+
+    let subaccount: Subaccount = address_data.into();
+
+    let result = ckbtc::update_balance(UpdateBalanceArgs {
+        owner: None,
+        subaccount: Some(subaccount.clone()),
+    })
+    .await
+    .map_err(|e| e.to_string())?;
+
+    let mut minted_success_utxo_status: Vec<BtcTransportRecord> = vec![];
+    let mut minted_success_amount = 0_u64;
+    for e in result {
+        match e {
+            UtxoStatus::ValueTooSmall(_) | UtxoStatus::Tainted(_) | UtxoStatus::Checked(_) => {
+                continue;
+            }
+            UtxoStatus::Minted {
+                block_index,
+                minted_amount,
+                utxo,
+            } => {
+                minted_success_utxo_status.push(
+                    BtcTransportRecord {
+                        block_index: block_index,
+                        minted_amount: minted_amount,
+                        utxo: utxo,
+                        ticket_id: None
+                    }
+                );
+                minted_success_amount += minted_amount;
+            }
+        }
+    }
+
+    // let mut btc_transport_record_list = get_btc_transport_records(osmosis_account_id);
+    // btc_transport_record_list.extend(minted_success_utxo_status);
+    // insert_btc_transport_records(osmosis_account_id, btc_transport_record_list);
+
+    approve_ckbtc_for_icp_custom(Some(subaccount), minted_success_amount.into())
+    .await
+    .map_err(|e| e.to_string())?;
+
+    let ticket_id = generate_ticket(
+        TOKEN_ID.to_string(),
+        TARGET_CHAIN_ID.to_string(),
+        nat_to_u128(minted_success_amount - Nat::from(2_u8) * Nat::from(10_u8)),
+        subaccount,
+    )
+    .await
+    .map_err(|e| e.to_string())?;
+
+    let mut btc_transport_record_list = get_btc_transport_records(osmosis_account_id.clone());
+    btc_transport_record_list.extend(minted_success_utxo_status.iter().map(|e| {
+        BtcTransportRecord {
+            block_index: e.block_index,
+            minted_amount: e.minted_amount,
+            utxo: e.utxo.clone(),
+            ticket_id: Some(ticket_id.clone())
+        }
+    }));
+    insert_btc_transport_records(osmosis_account_id, btc_transport_record_list);
+
+    // todo save ticket contained utxos
+
+    // todo should save transported utxo
+    // insert_executed_transaction_index(nat_to_u64(block_index), ticket_id);
+
     Ok(())
 }
 
@@ -40,7 +140,9 @@ pub async fn trigger_transaction(block_index: BlockIndex) -> std::result::Result
         "Transaction already executed"
     );
 
-    let transaction_response = get_ckbtc_transaction(block_index.clone()).await.map_err(|e| e.to_string())?;
+    let transaction_response = get_ckbtc_transaction(block_index.clone())
+        .await
+        .map_err(|e| e.to_string())?;
     assert_eq!(
         transaction_response.transactions.len(),
         1,
@@ -62,7 +164,9 @@ pub async fn trigger_transaction(block_index: BlockIndex) -> std::result::Result
         "Transaction not for this canister"
     );
 
-    approve_ckbtc_for_icp_custom(transfer.to.subaccount.clone(), transfer.amount.clone()).await.map_err(|e| e.to_string())?;
+    approve_ckbtc_for_icp_custom(transfer.to.subaccount.clone(), transfer.amount.clone())
+        .await
+        .map_err(|e| e.to_string())?;
 
     let ticket_id = generate_ticket(
         TOKEN_ID.to_string(),
@@ -70,11 +174,18 @@ pub async fn trigger_transaction(block_index: BlockIndex) -> std::result::Result
         nat_to_u128(transfer.amount - Nat::from(2_u8) * transfer.fee.unwrap_or(Nat::from(0_u8))),
         transfer.to.subaccount.unwrap(),
     )
-    .await.map_err(|e| e.to_string())?;
+    .await
+    .map_err(|e| e.to_string())?;
 
     insert_executed_transaction_index(nat_to_u64(block_index), ticket_id);
 
     Ok(())
+}
+
+#[post_upgrade]
+fn post_upgrade() {
+    let p = Principal::from_text("mqygn-kiaaa-aaaar-qaadq-cai").unwrap();
+    set_ckbtc_minter_principal(p);
 }
 
 ic_cdk::export_candid!();
