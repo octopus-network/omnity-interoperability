ebcebad4e08239c11a7762fea3da2f2d05ce1463
Bob(xsb.near) <389483673@qq.com>
2024-09-09 19:42:15 +0800

    Move the POC of cosmwasm to a separate repository (#126)

--- route/cosmwasm/src/cosmwasm.rs
+++
@@ -1,5 +0,0 @@
-pub mod client;
-pub mod port;
-pub mod rpc;
-
-pub type TxHash = String;
--- route/cosmwasm/src/cosmwasm/rpc/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod response;
-pub mod response_error;
-pub mod tx;
-pub mod version;
-pub mod wrapper;
