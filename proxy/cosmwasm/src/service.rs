ed2e94e4badbcc3412a708a45f57061fa8ec15bc
Bob(xsb.near) <389483673@qq.com>
2024-11-28 19:09:42 +0800

    feat[icp_customs&cosmwasm_proxy]: add ticket memo in generate ticket (#178)
    * feat[icp_customs&cosmwasm_proxy]: add ticket memo in generate ticket
    
    * chore: update assets

--- proxy/cosmwasm/src/service.rs
+++ proxy/cosmwasm/src/service.rs
@@ -120,6 +120,7 @@ pub async fn update_settings(
 #[update]
 pub async fn generate_ticket_from_subaccount(
     osmosis_account_id: String,
+    ticket_memo: Option<String>
 ) -> Result<TicketId, String> {
     let address_data = AddressData::try_from(osmosis_account_id.as_str())
         .map_err(|e| Errors::AccountIdParseError(osmosis_account_id.clone(), e.to_string()))
@@ -148,6 +149,7 @@ pub async fn generate_ticket_from_subaccount(
         setting.target_chain_id,
         ticket_amount,
         subaccount,
+        ticket_memo
     )
     .await
     .map_err(|e| e.to_string())?;
@@ -160,10 +162,13 @@ pub async fn generate_ticket_from_subaccount(
 }
 
 #[update]
-pub async fn update_balance_after_finalization(osmosis_account_id: String) {
+pub async fn update_balance_after_finalization(
+    osmosis_account_id: String, 
+    ticket_memo: Option<String>
+) {
 
     mutate_settings(|s| {
-        s.update_balances_jobs.push(UpdateBalanceJob::new(osmosis_account_id.clone()))
+        s.update_balances_jobs.push(UpdateBalanceJob::new(osmosis_account_id.clone(), ticket_memo))
     });
 
     log!(INFO, "Created update balance job for osmosis account id: {}", osmosis_account_id);
@@ -171,8 +176,8 @@ pub async fn update_balance_after_finalization(osmosis_account_id: String) {
 }
 
 #[update(guard = "is_controller")]
-pub async fn trigger_update_balance(osmosis_account_id: String) -> Result<TicketId, String> {
-    update_balance_and_generate_ticket(osmosis_account_id).await
+pub async fn trigger_update_balance(osmosis_account_id: String, ticket_memo: Option<String>) -> Result<TicketId, String> {
+    update_balance_and_generate_ticket(osmosis_account_id, ticket_memo).await
 }
 
 #[post_upgrade]
