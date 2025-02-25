31505b53a1550706f827ba5260c05628b71a1fed
stewart <16131485+StewartYe@users.noreply.github.com>
2025-02-25 21:39:17 +0800

    remote unused comments

--- customs/bitcoin_runes/src/runes_etching/topup.rs
+++ customs/bitcoin_runes/src/runes_etching/topup.rs
@@ -53,14 +53,12 @@ pub async fn topup(amt: u64,) -> Result<u128, String> {
     let cmc = Principal::from_text(CMC).unwrap();
     // icp ledger canister id
     let ledger = Principal::from_text(LEDGER_CANISTER).unwrap();
-    // 被充cycle的canister的canister id在cycle minting canister下的subaccount
     let subaccount = Subaccount::from(args.canister_id);
-    // cmc控制的subaccount
     let to = AccountIdentifier::new(&cmc, &subaccount);
 
-    // 1. transfer icp 给cmc
+    // 1. transfer icp to cmc
     let transfer_args = ic_ledger_types::TransferArgs {
-        memo: MEMO_TOP_UP_CANISTER, // 这个不能换，是写死的，topup就用这个memo
+        memo: MEMO_TOP_UP_CANISTER,
         amount: args.amount,
         fee: Tokens::from_e8s(10_000),
         from_subaccount: None,
@@ -73,16 +71,15 @@ pub async fn topup(amt: u64,) -> Result<u128, String> {
         .await
         .map_err(|e| format!("failed to call ledger: {:?}", e))?
         .map_err(|e| format!("ledger transfer error {:?}", e))?;
-    // 冲给谁，就用谁的canister id和subaccount
     // 2. notify cmc topup cycles
     let notify_arg = NotifyTopUp{
         block_index: block_idx,
         canister_id: args.canister_id
     };
+    if let Ok((res, )) = call::<_, (Result<Cycles, NotifyError>, )>(cmc, "notify_top_up", (notify_arg, )).await{
+        Ok(res.map_err(|e|format!("{:?}",e))?.0)
+    }else {
+        Err("send cycles error".to_string())
+    }
 
-    // 返回topup了多少cycle，如果有错误就返回notify error,没topup成功，icp会被cmc
-    // refund回原来打icp的account（bitcoin custom的account），不过会扣2个transfer icp
-    // 的手续费
-    let (res, ) = call::<_, (Result<Cycles, NotifyError>, )>(cmc, "notify_top_up", (notify_arg, )).await.unwrap();
-    Ok(res.map_err(|e|format!("{:?}",e))?.0)
 }
