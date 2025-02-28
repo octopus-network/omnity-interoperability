e5055e0476ebef2e1e961c3822edb64817ac4eb9
xsb <389483673@qq.com>
2025-02-28 19:48:50 +0800

    refactor[icp_customs]: replace handle_redeem_tickets with handle_failed_redeem_ticket

--- customs/icp/src/service.rs
+++ customs/icp/src/service.rs
@@ -5,14 +5,14 @@ use crate::state::{get_finalized_mint_token_request, mutate_state, read_state, C
 use crate::updates::generate_ticket::{GenerateTicketError, GenerateTicketOk, GenerateTicketReq};
 use crate::{hub, lifecycle, periodic_task, updates, PERIODIC_TASK_INTERVAL};
 use candid::Principal;
+use ic_canister_log::log;
 use ic_canisters_http_types::{HttpRequest, HttpResponse};
 use ic_cdk_macros::{init, post_upgrade, query, update};
 use ic_cdk_timers::set_timer_interval;
 use ic_ledger_types::{AccountIdentifier, Subaccount};
+use omnity_types::ic_log::{ERROR, INFO};
 use omnity_types::MintTokenStatus::{Finalized, Unknown};
 use omnity_types::{Chain, MintTokenStatus, Seq, Ticket, TicketId, Token};
-use ic_canister_log::log;
-use omnity_types::ic_log::{ERROR, INFO};
 
 pub fn is_controller() -> Result<(), String> {
     if ic_cdk::api::is_controller(&ic_cdk::caller()) {
@@ -46,9 +46,11 @@ fn check_anonymous_caller() {
 }
 
 #[update]
-async fn generate_ticket_v2(args: GenerateTicketReq) -> Result<GenerateTicketOk, GenerateTicketError> {
+async fn generate_ticket_v2(
+    args: GenerateTicketReq,
+) -> Result<GenerateTicketOk, GenerateTicketError> {
     check_anonymous_caller();
-    match updates::generate_ticket_v2(args.clone()).await  {
+    match updates::generate_ticket_v2(args.clone()).await {
         Ok(r) => {
             log!(
                 INFO,
@@ -56,16 +58,12 @@ async fn generate_ticket_v2(args: GenerateTicketReq) -> Result<GenerateTicketOk,
                 args,
                 r
             );
-            return Ok(r)
-        },
+            return Ok(r);
+        }
         Err(err) => {
-            log!(
-                ERROR,
-                "failed to generate_ticket_v2, args: {:?}",
-                args
-            );
-            return Err(err)
-        },
+            log!(ERROR, "failed to generate_ticket_v2, args: {:?}", args);
+            return Err(err);
+        }
     }
 }
 
@@ -76,16 +74,18 @@ async fn generate_ticket(args: GenerateTicketReq) -> Result<GenerateTicketOk, Ge
 }
 
 #[update(guard = "is_controller")]
-async fn refund_icp(principal: Principal)->Result<(ic_ledger_types::BlockIndex, u64), String> {
+async fn refund_icp(principal: Principal) -> Result<(ic_ledger_types::BlockIndex, u64), String> {
     updates::generate_ticket::refund_icp_from_subaccount(principal).await
 }
 
 #[update(guard = "is_controller")]
-async fn resend_ticket(ticket_id: TicketId)->Result<(), String>{
-    let (failed_tickets, hub_principal) = read_state(|s| (
-        s.failed_send_to_hub_ticket_list.clone(), 
-        s.hub_principal.clone()
-    ));
+async fn resend_ticket(ticket_id: TicketId) -> Result<(), String> {
+    let (failed_tickets, hub_principal) = read_state(|s| {
+        (
+            s.failed_send_to_hub_ticket_list.clone(),
+            s.hub_principal.clone(),
+        )
+    });
 
     if let Some(ticket) = failed_tickets.iter().find(|t| t.ticket_id.eq(&ticket_id)) {
         if let Err(err) = hub::send_ticket(hub_principal, ticket.clone()).await {
@@ -97,20 +97,16 @@ async fn resend_ticket(ticket_id: TicketId)->Result<(), String>{
             );
             return Err(err.to_string());
         } else {
-            mutate_state(
-                |state| {
-                    state.failed_send_to_hub_ticket_list.retain(|e| e.ticket_id.ne(&ticket_id));
-                }
-            );
-            log!(
-                INFO,
-                "Success to resend ticket: {}",
-                ticket.ticket_id
-            )
+            mutate_state(|state| {
+                state
+                    .failed_send_to_hub_ticket_list
+                    .retain(|e| e.ticket_id.ne(&ticket_id));
+            });
+            log!(INFO, "Success to resend ticket: {}", ticket.ticket_id)
         }
     }
 
-    return Ok(())
+    return Ok(());
 }
 
 #[query]
@@ -166,28 +162,33 @@ pub async fn query_hub_tickets(from: u64, limit: u64) -> Vec<(Seq, Ticket)> {
 }
 
 #[update(guard = "is_controller")]
-pub async fn handle_ticket(seq: u64)->Result<u64, String> {
-    let hub_principal = read_state(|s| s.hub_principal);
-    let r = hub::query_tickets(hub_principal, seq, 1)
-        .await
-        .unwrap()
-        .first()
-        .unwrap()
-        .clone();
-    match super::handle_redeem_ticket(&r.1).await {
-        Ok(block_index) => {
-            log::info!(
-                "[handle_ticket] process successful for ticket: {}, block_index: {}",
-                r.1,
-                block_index
-            );
+pub async fn handle_failed_redeem_ticket(seq: u64) -> Result<u64, String> {
 
-            Ok(block_index)
-        },
-        Err(e) => {
-            log::error!("[handle_ticket] failed to process ticket: {}, error: {}", r.1, e);
-            Err(e)
-        },
+    if let Some((_, ticket)) =
+        read_state(|s| s.failed_redeem_ticket_list.clone().into_iter().find(|e| e.0.to_owned().eq(&seq)))
+    {
+        match super::handle_redeem_ticket(&ticket).await {
+            Ok(block_index) => {
+                mutate_state(|s| {
+                    s.failed_redeem_ticket_list.retain(
+                        |e| e.1.ticket_id.ne(&ticket.ticket_id)
+                    );
+                });
+                log::info!(
+                    "Success to handle_failed_redeem_ticket: {}, block_index: {}",
+                    ticket,
+                    block_index
+                );
+
+                Ok(block_index)
+            }
+            Err(e) => {
+                log::error!("Failed to process ticket: {}, error: {}", ticket, e);
+                Err(e)
+            }
+        }
+    } else {
+        return Err("Ticket not found".to_string())
     }
 }
 
@@ -208,4 +209,3 @@ fn mint_token_status(ticket_id: TicketId) -> MintTokenStatus {
 
 // Enable Candid export
 ic_cdk::export_candid!();
-
